c92b181cfe1dbd9201a6aa9821895286baf6fe5c
M.Amine Oufroukhi <160159553+M-Amine-oufroukhi@users.noreply.github.com>
2024-05-02 02:44:22 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,7 +1,8 @@
---
+--Retrieve the Patient_id and ages of all patients.
 SELECT Patient_id,
        TIMESTAMPDIFF(YEAR, `D.O.B`, CURDATE()) AS Age
 FROM diabetes_prediction;
---
+--Calculate the average BMI of patients
 SELECT AVG(bmi) AS average_bmi
 FROM diabetes_prediction;
+--List patients in descending order of blood glucose levels
